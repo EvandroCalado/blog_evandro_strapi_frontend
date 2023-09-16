a7d58ff352868ce2c50e67ee95a86e56669a3e9a
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-16 15:54:09 -0300

    :chore:  add gridTitle props

--- src/components/PostGrid/index.tsx
+++ src/components/PostGrid/index.tsx
@@ -1,17 +1,29 @@
 import { Posts as StrapiPosts } from '../../types/strapi';
+import Heading from '../Heading';
 import PostCard from '../PostCard';
+import PostLatest from '../PostLatest';
 import * as Styled from './styles';
 
 export interface PostGridProps {
   posts: StrapiPosts;
+  gridTitle?: string;
 }
 
-export default function PostGrid({ posts }: PostGridProps) {
+export default function PostGrid({
+  posts,
+  gridTitle = 'Postagens Recentes',
+}: PostGridProps) {
+  const postsSlice = posts.data.slice(1);
+
   return (
     <Styled.Container>
-      {posts.data.map((post) => (
-        <PostCard key={post.id} post={post} />
-      ))}
+      <Heading>{gridTitle}</Heading>
+      <Styled.GridContainer>
+        {<PostLatest post={posts.data[0]} />}
+        {postsSlice.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </Styled.GridContainer>
     </Styled.Container>
   );
 }
