9ac0c62b26a39ac3fdbe50742431ef2e9b287779
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-16 15:57:25 -0300

    :chore: create queries

---
+++ src/graphql/queries.ts
@@ -0,0 +1,237 @@
+import { gql } from 'graphql-request';
+import { FRAGMENTS } from './fragments';
+
+export const QUERIES_GET_POSTS = gql`
+  ${FRAGMENTS}
+
+  query GET_POSTS(
+   $sort: [String] = "createdAt:desc",
+   $start: Int = 0,
+   $limit: Int = 10
+  ) {
+  setting {
+    data {
+      id
+      attributes {
+        ...setting
+      }
+    }
+  }
+  posts(sort: $sort pagination: { start: $start, limit: $limit }) {
+    data {
+      id
+      attributes {
+        ...post
+      }
+    }
+    meta {
+      pagination {
+        total
+        page
+        pageSize
+        pageCount
+      }
+    }
+  }
+}
+`;
+
+export const QUERIES_GET_POSTS_BY_SLUG = gql`
+  ${FRAGMENTS}
+
+  query GET_POSTS_BY_SLUG(
+  $postSlug: String
+  $sort: [String] = "createdAt:desc"
+  $start: Int = 0
+  $limit: Int = 10
+) {
+  setting {
+    data {
+      id
+      attributes {
+        ...setting
+      }
+    }
+  }
+  posts(
+    sort: $sort
+    pagination: { start: $start, limit: $limit }
+    filters: { slug: { eq: $postSlug } }
+  ) {
+    data {
+      id
+      attributes {
+        ...post
+      }
+    }
+    meta {
+      pagination {
+        total
+        page
+        pageSize
+        pageCount
+      }
+    }
+  }
+}
+`;
+
+export const QUERIES_GET_POSTS_BY_AUTHOR = gql`
+  ${FRAGMENTS}
+
+  query GET_POSTS_BY_AUTHOR(
+  $authorSlug: String
+  $sort: [String] = "createdAt:desc"
+  $start: Int = 0
+  $limit: Int = 10
+) {
+  setting {
+    data {
+      id
+      attributes {
+        ...setting
+      }
+    }
+  }
+  posts(
+    sort: $sort
+    pagination: { start: $start, limit: $limit }
+    filters: { author: { slug: { eq: $authorSlug } } }
+  ) {
+    data {
+      id
+      attributes {
+        ...post
+      }
+    }
+    meta {
+      pagination {
+        total
+        page
+        pageSize
+        pageCount
+      }
+    }
+  }
+}
+`;
+
+export const QUERIES_GET_POSTS_BY_CATEGORY = gql`
+  ${FRAGMENTS}
+
+  query GET_POSTS_BY_CATEGORY(
+  $categorySlug: String
+  $sort: [String] = "createdAt:desc"
+  $start: Int = 0
+  $limit: Int = 10
+) {
+  setting {
+    data {
+      id
+      attributes {
+        ...setting
+      }
+    }
+  }
+  posts(
+    sort: $sort
+    pagination: { start: $start, limit: $limit }
+    filters: { categories: { slug: { eq: $categorySlug } } }
+  ) {
+    data {
+      id
+      attributes {
+        ...post
+      }
+    }
+    meta {
+      pagination {
+        total
+        page
+        pageSize
+        pageCount
+      }
+    }
+  }
+}
+`;
+
+export const QUERIES_GET_POSTS_BY_TAG = gql`
+  ${FRAGMENTS}
+
+  query GET_POSTS_BY_TAG(
+  $tagSlug: String
+  $sort: [String] = "createdAt:desc"
+  $start: Int = 0
+  $limit: Int = 10
+) {
+  setting {
+    data {
+      id
+      attributes {
+        ...setting
+      }
+    }
+  }
+  posts(
+    sort: $sort
+    pagination: { start: $start, limit: $limit }
+    filters: { tags: { slug: { eq: $tagSlug } } }
+  ) {
+    data {
+      id
+      attributes {
+        ...post
+      }
+    }
+    meta {
+      pagination {
+        total
+        page
+        pageSize
+        pageCount
+      }
+    }
+  }
+}
+`;
+
+export const QUERIES_GET_POSTS_BY_TITLE = gql`
+  ${FRAGMENTS}
+
+  query GET_POSTS_BY_TITLE(
+  $postTitle: String
+  $sort: [String] = "createdAt:desc"
+  $start: Int = 0
+  $limit: Int = 10
+) {
+  setting {
+    data {
+      id
+      attributes {
+        ...setting
+      }
+    }
+  }
+  posts(
+    sort: $sort
+    pagination: { start: $start, limit: $limit }
+    filters: { title: { containsi: $postTitle } }
+  ) {
+    data {
+      id
+      attributes {
+        ...post
+      }
+    }
+    meta {
+      pagination {
+        total
+        page
+        pageSize
+        pageCount
+      }
+    }
+  }
+}
+`;
