7ed4078f3092cad8e622b8197ba51b789b12df81
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-16 15:59:30 -0300

    :chore: create page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,61 @@
-import Error404 from '../components/Error404';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { QUERIES_GET_POSTS } from '../graphql/queries';
+import Posts from '../templates/Posts';
+import {
+  Posts as StrapiPosts,
+  Setting as StrapiSetting,
+} from '../types/strapi';
+import {
+  LoadPostsVariables,
+  defaultVariables,
+  loadPosts,
+} from '../utils/load-posts';
 
-export default function Home() {
+interface HomeProps {
+  posts: StrapiPosts;
+  setting: StrapiSetting;
+  variables?: LoadPostsVariables;
+}
+
+export default function Home({ posts, setting, variables }: HomeProps) {
   return (
-    <div>
-      <h1>Home</h1>
-      <Error404 />
-    </div>
+    <>
+      <Head>
+        <title>{setting.data.attributes.blogName}</title>
+        <meta
+          name="description"
+          content={setting.data.attributes.blogDescription}
+        />
+      </Head>
+      <Posts posts={posts} setting={setting} variables={variables} />
+    </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  let data = null;
+
+  try {
+    data = await loadPosts(QUERIES_GET_POSTS);
+  } catch (error) {
+    data = null;
+  }
+
+  if (!data || !data.posts || !data.posts.data.length) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      posts: data.posts,
+      setting: data.setting,
+      variables: {
+        ...defaultVariables,
+      },
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
