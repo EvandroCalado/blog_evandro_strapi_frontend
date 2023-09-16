4c73d8c8d18ba330304012bb90a52e16e2050798
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-16 15:52:37 -0300

    :chore: create LinkButton component
    create LinkButton component with styles and stories

---
+++ src/components/LinkButton/index.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import icons from '../../icons';
+import * as Styled from './styles';
+
+export interface LinkButtonProps {
+  children: React.ReactNode;
+  href: string;
+}
+
+export default function LinkButton({ children, href }: LinkButtonProps) {
+  return (
+    <Styled.Container>
+      <Link href={href}>
+        {children} {icons.arrowRght}
+      </Link>
+    </Styled.Container>
+  );
+}
