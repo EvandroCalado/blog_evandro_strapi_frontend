bcee0027716037629c41d3ecbb751816ca1f3517
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-18 09:48:46 -0300

    :chore:  create PostNotFound component
    create PostNotFound component with styles and stories

---
+++ src/components/PostNotFound/index.tsx
@@ -0,0 +1,10 @@
+import Heading from '../Heading';
+import * as Styled from './styles';
+
+export default function PostNotFound() {
+  return (
+    <Styled.Container>
+      <Heading>Post não encontrado !</Heading>
+    </Styled.Container>
+  );
+}
---
+++ src/components/PostNotFound/styles.ts
@@ -0,0 +1,15 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    display: flex;
+    height: 54.6rem;
+    align-items: center;
+    justify-content: center;
+
+    @media screen and ${theme.media.lteOrEqSmall} {
+      height: 30rem;
+    }
+`}
+`;
