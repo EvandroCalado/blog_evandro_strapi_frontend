de04841b4a9512e6951c386ed6b6bd31817bc045
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-18 09:51:45 -0300

    :chore: create Search page

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,82 @@
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { QUERIES_GET_POSTS_BY_TITLE } from '../../graphql/queries';
+import Posts from '../../templates/Posts';
+import {
+  Posts as StrapiPosts,
+  Setting as StrapiSetting,
+} from '../../types/strapi';
+import {
+  LoadPostsVariables,
+  defaultVariables,
+  loadPosts,
+} from '../../utils/load-posts';
+
+interface SearchPageProps {
+  posts: StrapiPosts;
+  setting: StrapiSetting;
+  variables?: LoadPostsVariables;
+  route?: string;
+}
+
+export default function SearchPage({
+  posts,
+  setting,
+  variables,
+  route,
+}: SearchPageProps) {
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>{`Pesquisa: ${router.query.q}`}</title>
+      </Head>
+      <Posts
+        posts={posts}
+        setting={setting}
+        gridTitle={router.query.q as string}
+        variables={variables}
+        route={route}
+      />
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  let data = null;
+  const query = ctx.query?.q || '';
+
+  if (!query) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const variables = { postTitle: query as string };
+
+  try {
+    data = await loadPosts(QUERIES_GET_POSTS_BY_TITLE, variables);
+  } catch (error) {
+    data = null;
+  }
+
+  if (!data || !data.posts) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      posts: data.posts,
+      setting: data.setting,
+      variables: {
+        ...defaultVariables,
+        ...variables,
+      },
+      route: 'title',
+    },
+  };
+};
