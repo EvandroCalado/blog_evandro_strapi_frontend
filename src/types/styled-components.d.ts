963b2a4855f8c4ec0a8152e72ff45a3d1447b63e
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-18 09:54:40 -0300

    :fix: add darkTheme

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -1,6 +1,6 @@
-import { lightTheme } from '../styles/theme';
+import { darkTheme, lightTheme } from '../styles/theme';
 
-type Theme = typeof lightTheme;
+type Theme = typeof lightTheme | typeof darkTheme;
 
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {}
