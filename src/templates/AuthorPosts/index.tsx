8f32d4985855e89eb692bf4c270654a0c4cb3a09
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-18 09:53:15 -0300

    :chore: add pagination and post score

--- src/templates/AuthorPosts/index.tsx
+++ src/templates/AuthorPosts/index.tsx
@@ -1,26 +1,77 @@
+import { useState } from 'react';
 import AuthorPerfil from '../../components/AuthorPerfil';
+import Button from '../../components/Button';
 import PostGrid from '../../components/PostGrid';
 import {
   Posts as StrapiPosts,
   Setting as StrapiSetting,
 } from '../../types/strapi';
+import { LoadPostsVariables, loadPosts } from '../../utils/load-posts';
+import { paginationQueries } from '../../utils/pagination-queries';
 import Base from '../Base';
+import * as Styled from './styles';
 
 export interface AuthorPostsProps {
   posts: StrapiPosts;
   setting: StrapiSetting;
   gridTitle?: string;
+  variables?: LoadPostsVariables;
+  route?: string;
 }
 
 export default function AuthorPosts({
   posts,
   setting,
   gridTitle,
+  variables,
+  route,
 }: AuthorPostsProps) {
+  const [statePosts, setStatePosts] = useState(posts);
+  const [stateVariables, setStateVariables] = useState(variables);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [noMorePosts, setNoMorePosts] = useState(false);
+
+  const handleLoadMorePosts = async () => {
+    setButtonDisabled(true);
+
+    const newVariables = {
+      ...stateVariables,
+      start: stateVariables!.start! + stateVariables!.limit!,
+      limit: stateVariables!.limit,
+    };
+
+    const morePosts = await loadPosts(
+      paginationQueries[route! as keyof typeof paginationQueries],
+      newVariables,
+    );
+
+    if (!morePosts || !morePosts.posts.data || !morePosts.posts.data.length) {
+      setNoMorePosts(true);
+      return;
+    }
+
+    const posts = {
+      ...statePosts,
+      data: [...statePosts.data, ...morePosts.posts.data],
+    };
+
+    setButtonDisabled(false);
+    setStateVariables(newVariables);
+    setStatePosts(posts);
+  };
+
   return (
     <Base setting={setting}>
-      <AuthorPerfil author={posts.data[0].attributes.author} />
-      <PostGrid posts={posts} gridTitle={gridTitle} />
+      <AuthorPerfil
+        author={posts.data[0].attributes.author}
+        pagination={posts.meta.pagination}
+      />
+      <PostGrid posts={statePosts} gridTitle={gridTitle} />
+      <Styled.ButtonContainer>
+        <Button onClick={handleLoadMorePosts} disabled={buttonDisabled}>
+          {noMorePosts ? 'Sem mais posts' : 'Carregar mais'}
+        </Button>
+      </Styled.ButtonContainer>
     </Base>
   );
 }
