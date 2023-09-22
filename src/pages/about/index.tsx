958000bffdea24d9057af97a6efd116035dfae44
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-21 23:34:07 -0300

    :refactor:  change About for AboutPage

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,32 +1,32 @@
 import { GetStaticProps } from 'next';
-import ComingSoon from '../../components/ComingSoon';
-import { QUERIES_GET_POSTS } from '../../graphql/queries';
-import Base from '../../templates/Base';
-import { Setting } from '../../types/strapi';
+import { QUERIES_GET_ABOUT, QUERIES_GET_POSTS } from '../../graphql/queries';
+import AboutTemplate from '../../templates/AboutTemplate';
+import { Setting as SettingStrapi } from '../../types/strapi';
+import { About as AboutStrapi } from '../../types/strapi-about';
 import { loadPosts } from '../../utils/load-posts';
 
-interface AboutProps {
-  setting: Setting;
+interface AboutPageProps {
+  setting: SettingStrapi;
+  about: AboutStrapi;
 }
 
-export default function About({ setting }: AboutProps) {
-  return (
-    <Base setting={setting}>
-      <ComingSoon />
-    </Base>
-  );
+export default function AboutPage({ setting, about }: AboutPageProps) {
+  return <AboutTemplate setting={setting} about={about} />;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   let data = null;
+  let aboutsData = null;
 
   try {
     data = await loadPosts(QUERIES_GET_POSTS);
+    aboutsData = await loadPosts(QUERIES_GET_ABOUT);
   } catch (error) {
     data = null;
+    aboutsData = null;
   }
 
-  if (!data) {
+  if (!data || !aboutsData) {
     return {
       notFound: true,
     };
@@ -35,6 +35,7 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       setting: data.setting,
+      about: aboutsData.abouts.data[0],
     },
     revalidate: 60 * 60 * 24,
   };
