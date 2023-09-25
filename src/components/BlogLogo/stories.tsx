4635e7ee1a57feb6123145c5ffcb77cee78d3a0f
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-25 15:41:50 -0300

    :story: change category

--- src/components/BlogLogo/stories.tsx
+++ src/components/BlogLogo/stories.tsx
@@ -5,7 +5,7 @@ import mock from '../../utils/db.json';
 const setting = mock.data.setting;
 
 export default {
-  title: 'Components/BlogLogo',
+  title: 'Blog/BlogLogo',
   component: BlogLogo,
   args: {
     setting,
