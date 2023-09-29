a34b30bce5576956291c07bf0fbce02ce570d45c
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-29 10:51:02 -0300

    :chore: add icons

---
+++ src/icons/icon-article.tsx
@@ -0,0 +1,20 @@
+export const IconArticle = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-article"
+    width={25}
+    height={25}
+    viewBox="0 0 24 24"
+    strokeWidth={1}
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
+    <path d="M7 8h10" />
+    <path d="M7 12h10" />
+    <path d="M7 16h10" />
+  </svg>
+);
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,5 +1,6 @@
 export * from './icon-arrowright';
 export * from './icon-arrowup';
+export * from './icon-article';
 export * from './icon-clock';
 export * from './icon-dark';
 export * from './icon-github';
@@ -8,4 +9,7 @@ export * from './icon-light';
 export * from './icon-linkedin';
 export * from './icon-menu';
 export * from './icon-search';
+export * from './icon-storybook';
+export * from './icon-strapi';
+export * from './icon-vercel';
 export * from './icon-x';
