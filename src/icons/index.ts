1fab008582ce9ba7ec1161858df60ddc7a199413
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-29 23:39:36 -0300

    :chore: add icon-link

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -6,6 +6,7 @@ export * from './icon-dark';
 export * from './icon-github';
 export * from './icon-instagram';
 export * from './icon-light';
+export * from './icon-link';
 export * from './icon-linkedin';
 export * from './icon-menu';
 export * from './icon-search';
